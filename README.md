326df5346040ddb49c3b3f9e27a8b3eb331677a7
sreejakailas <sreejakailas95@gmail.com>
2016-06-03 13:05:43 +0530

    First commit, added all files

---
+++ README.md
@@ -0,0 +1,4 @@
+# workshop
+# workshop
+# workshop
+#test
---
+++ register/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from braces.views import LoginRequiredMixin, AnonymousRequiredMixin
+from register.forms import *
+from django.views.generic.edit import FormView,UpdateView
+from django.core.urlresolvers import reverse_lazy
+
+# Create your views here.
+class Home(TemplateView):
+   template_name="index.html"
+
+class UserRegistrationView(AnonymousRequiredMixin, FormView):
+    template_name = "register_user.html"
+    authenticated_redirect_url = reverse_lazy(u"home")
+    form_class = UserRegistrationForm
+    success_url = '/register/user/success/'
+
+    def form_valid(self, form):
+        form.save()
+        return FormView.form_valid(self, form)
